94d80da763cb15611cf8d1560038246219fcde87
Mateusz <66961646+Regnar110@users.noreply.github.com>
2025-02-09 22:03:34 +0100

    Implementing font loader and transfering ttf to json fonts

--- src/planets/Sun.ts
+++ src/planets/Sun.ts
@@ -1,18 +1,36 @@
 import * as THREE from 'three';
 import sunTexture from '../textures/sun.jpg';
+import { FontLoader, TextGeometry } from 'three/examples/jsm/Addons.js';
 
 export class StarSun {
 	private sunObject: THREE.Object3D;
 	private geometry: THREE.SphereGeometry;
 	private material: THREE.MeshBasicMaterial;
 	private mesh: THREE.Mesh;
+	private fontLoader: FontLoader = new FontLoader();
+	private textGeometry: TextGeometry
 	
 	constructor(textureLoader: THREE.TextureLoader) {
 		this.geometry = new THREE.SphereGeometry(20, 14, 14);
 		this.material = new THREE.MeshBasicMaterial({ map: textureLoader.load(sunTexture) });
 		this.mesh = new THREE.Mesh(this.geometry, this.material);
 		this.sunObject = new THREE.Object3D();
-		this.sunObject.add(this.mesh)
+		this.sunObject.add(this.mesh);
+		this.fontLoader.load('/fonts/Roboto,Space_Mono/Space_Mono/SpaceMono_Regular.json', (font) => {
+			console.log(font)
+			 this.textGeometry = new TextGeometry('Sun', {
+				font: font,
+				size: 24,
+				depth: 5,
+				curveSegments: 12,
+				bevelEnabled: true,
+				bevelThickness: 10,
+				bevelSize: 8,
+				bevelOffset: 0,
+				bevelSegments: 5
+			})
+		}, undefined, (e) => console.log(e))
+
 	}
 
 	get object() {
